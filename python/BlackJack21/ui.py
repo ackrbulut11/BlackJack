18c1f446911f0f8e5745b9e9b2389a834ee55991
Ahmetcan Karabulut <ackrbulut11@gmail.com>
2025-02-07 04:15:44 +0300

    unfinished UI with class

--- python/BlackJack21/ui.py
+++ python/BlackJack21/ui.py
@@ -1,34 +1,33 @@
 import customtkinter as ctk
+from game_logic import *
 
-def startUI():
 
-    def hit():
+class UI:
+
+    def __init__(self):
+        # app frame
+        self.root = ctk.CTk()
+        self.root.title("BlackJack")
+        self.root.geometry("720x400")
+
+        self.game = BlackJack()
+
+        # buttons
+        self.hit_button = ctk.CTkButton(self.root, text = "Hit", command=self.game.hit()) # player hit or dealer hit
+        self.hit_button.grid(row=0, column=0, padx = 30, pady = 20)
+
+        self.stand_button = ctk.CTkButton(self.root, text = "Stand", command = self.game.stand())
+        self.stand_button.grid(row=0, column=1, padx = 30, pady = 20)
+
+
+    def start(self):
         pass
 
-    def stand():
-        run = ctk.CTkLabel(app, text= "run" )
-        run.pack(padx = 10, pady=10)
-
-    # system
-    ctk.set_appearance_mode("System")
-    ctk.set_default_color_theme("green")
-
-    # app frame
-    app = ctk.CTk()
-    app.title("BlackJack")
-    app.geometry("720x480")
-
-    # buttons
-    hit = ctk.CTkButton(app, text = "Hit", command=hit)
-    hit.grid(row=0, column=0, padx = 30, pady = 20)
-
-    stand = ctk.CTkButton(app, text = "Stand", command=stand)
-    stand.grid(row=0, column=1, padx = 30, pady = 20)
-
     # run
-    app.mainloop()
+    def run(self):
+        self.root.mainloop()
 
-startUI()
+
 
 
 
